13edfc5aed92ff7a1aa5305376794d474b03d523
Shaun <seyyes2000@hotmail.com>
2017-02-23 15:18:59 -0500

    crop photos; make 2 pics per row

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
-function menu() {
-    var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += " responsive";
-        }
-        else {
-            x.className = "topnav";
-        }
-}
+(
+	function menu() {
+		var x = document.getElementById("myTopnav");
+			if (x.className === "topnav") {
+				x.className += " responsive";
+			}
+			else {
+				x.className = "topnav";
+			}
+	}
+)
